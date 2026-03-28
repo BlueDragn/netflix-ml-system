e5ac298e5968fa9603c7914042b20802e3ae1a50
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-03-28 11:06:13 +0530

    Implement data loading module
    Created load_data function to load cleaned dataset from csv

--- src/data_loader.py
+++ src/data_loader.py
@@ -1,3 +1,10 @@
 '''
-Load  dataset return the dataframe 
+Load  dataset return the dataframe
 '''
+import pandas as pd
+def load_data():
+    df = pd.read_csv("data/cleaned_data.csv")
+    return df
+
+
+
