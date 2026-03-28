8f7b0790878385482a544d5e02ec1f79b0680513
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-03-28 23:54:32 +0530

    Initialize recommender module
    Implement basic structure for recommendation logic
    
    Added index lookup and similarity score extraction
    
    Next step: sort scores and return top recommendation

--- src/recommender.py
+++ src/recommender.py
@@ -3,3 +3,13 @@ Main Logic:
 Input title -> Find Index -> Get Vector -> Call similarity -> Return top results
 
 '''
+def find_index(df, title):
+    return  df[df["title"] == title].index[0]
+
+
+
+def recommend(df,sim_matrix,title):
+    idx = find_index(df, title)
+    scores = sim_matrix[idx]
+    return scores
+
