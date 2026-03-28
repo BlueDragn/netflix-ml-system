21e0c2fa01964d2c420d11e71eee9cc3af86d070
BlueDragn <76735748+BlueDragn@users.noreply.github.com>
2026-03-28 22:27:06 +0530

    Implement TF-IDF vectorization module
    Created vectorize function to convert combined_text into TF-IDF matrix
    
    Used Tfidfvectorizer with fir_transform for efficient processing
    
    Returned both tfidf_matrix and trained vectorizer for downstream usage

--- src/vectorizer.py
+++ src/vectorizer.py
@@ -3,4 +3,14 @@ Handle TF-IDF:
 - fit()
 - Transform()
 - return tfidf_matrix + vectorizer
+
 '''
+
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+def vectorize(df):
+    text_data = df["combined_text"]
+    vectorizer = TfidfVectorizer()
+    tfidf_matrix = vectorizer.fit_transform(text_data)
+    return tfidf_matrix, vectorizer
+
